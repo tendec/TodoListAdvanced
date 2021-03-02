c29b7fc462d3cd499a1a4848ddfa650ce87b6816
DESKTOP-SO57OLA\Le Tan Dat <letandat260199@gmail.com>
2021-03-02 17:08:05 +0700

    Card works

--- cards.js
+++ cards.js
@@ -1,19 +1,135 @@
+let todoStorageArr = [];
+let colorStorageArr = [];
 let btnAddTodoItem = document.getElementById("btn-addTodo");
 btnAddTodoItem.onclick = function () {
     let inputElement = document.getElementById("addTodo-field");
-    if (inputElement.value == "") {
-        alert("Please type a todo!");
-    } else {
-        createTodoItem(inputElement.value);
-        /* todoStorageArr.push(inputElement.value);
-        colorStorageArr.push("black");
-        saveData(); */
+    if (todoStorageArr.length == 5) {
+        alert("Please complete the others first!");
         inputElement.value = "";
+    } else {
+        if (inputElement.value == "") {
+            alert("Please type a todo!");
+        } else {
+            createTodoItem(inputElement.value);
+            todoStorageArr.push(inputElement.value);
+            colorStorageArr.push("black");
+            saveData();
+            inputElement.value = "";
+            setPriority();
+        }
     }
+}
+
+let sub = document.getElementById("addTodo-field");
+sub.addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        event.preventDefault();
+        btnAddTodoItem.click();
+    }
+});
+
+window.onload = function () {
+    loadData();
+    for (let i = 0; i < todoStorageArr.length; i++) {
+        createTodoItem(todoStorageArr[i]);
+        if (colorStorageArr[i] == "red") {
+            red[i].parentNode.children[1].classList.add("redIsHigh");
+        } else if (colorStorageArr[i] == "yellow") {
+            yellow[i].parentNode.children[1].classList.add("yellowIsMedium");
+        } else if (colorStorageArr[i] == "green") {
+            green[i].parentNode.children[1].classList.add("greenIsLow");
+        }
+    }
+    setPriority();
+}
+
+function createCardItem() {
+
 }
 
 function createTodoItem(value) {
     document.getElementById("card-todos").innerHTML +=
         "<li><i class='fas fa-check' onclick='onCheckboxClick(this)'></i>" +
-        "<span class='todoItems'>" + value + "</span>" + "</li>";
+        "<span class='todoItems'>" + value + "</span>" +
+        "<button class='btnPriority highBtn'>High</button><button class='btnPriority mediumBtn'>Medium</button><button class='btnPriority lowBtn'>Low</button>" +
+        "</li>";
+}
+
+let green = document.getElementsByClassName("lowBtn");
+let yellow = document.getElementsByClassName("mediumBtn");
+let red = document.getElementsByClassName("highBtn");
+function setPriority() {
+    for (let i = 0; i < red.length; i++) {
+        red[i].onclick = function () {
+            if (red[i].parentNode.children[1].classList.contains("yellowIsMedium") || red[i].parentNode.children[1].classList.contains("greenIsLow")) {
+                red[i].parentNode.children[1].classList.remove("yellowIsMedium", "greenIsLow");
+            };
+            red[i].parentNode.children[1].classList.add("redIsHigh");
+            colorStorageArr[i] = "red";
+            saveData();
+        }
+    };
+    for (let j = 0; j < yellow.length; j++) {
+        yellow[j].onclick = function () {
+            if (yellow[j].parentNode.children[1].classList.contains("redIsHigh") || yellow[j].parentNode.children[1].classList.contains("greenIsLow")) {
+                yellow[j].parentNode.children[1].classList.remove("redIsHigh", "greenIsLow");
+            };
+            yellow[j].parentNode.children[1].classList.add("yellowIsMedium");
+            colorStorageArr[j] = "yellow";
+            saveData();
+        }
+    }
+    for (let o = 0; o < green.length; o++) {
+        green[o].onclick = function () {
+            if (green[o].parentNode.children[1].classList.contains("yellowIsMedium") || green[o].parentNode.children[1].classList.contains("redIsHigh")) {
+                green[o].parentNode.children[1].classList.remove("yellowIsMedium", "redIsHigh");
+            };
+            green[o].parentNode.children[1].classList.add("greenIsLow");
+            colorStorageArr[o] = "green";
+            saveData();
+        }
+    }
+}
+
+function onCheckboxClick(element) {
+    const content = element.parentNode.querySelector(".todoItems").innerText;
+    for (let i = 0; i < todoStorageArr.length; i++) {
+        if (todoStorageArr[i] == content) {
+            todoStorageArr.splice(i, 1);
+            colorStorageArr.splice(i, 1);
+            saveData();
+        }
+    }
+    element.parentNode.classList.add("deleteLi");
+    setTimeout(function () {
+        deleteElement(element.parentNode);
+    }, 300)
+}
+
+function deleteElement(element) {
+    container = document.querySelector("#card-todos");
+    container.removeChild(element);
+}
+
+function saveData() {
+    if (todoStorageArr.length == 0) {
+        localStorage.removeItem("todoData");
+        localStorage.removeItem("colorData");
+    } else {
+        localStorage.setItem("todoData", JSON.stringify(todoStorageArr));
+        localStorage.setItem("colorData", JSON.stringify(colorStorageArr));
+    }
+}
+
+function loadData() {
+    const dataTodo = localStorage.getItem("todoData");
+    const dataColor = localStorage.getItem("colorData");
+    todoStorageArr = dataTodo == null ? [] : JSON.parse(dataTodo);
+    colorStorageArr = dataColor == null ? [] : JSON.parse(dataColor);
+}
+
+function unfocusField(element) {
+    if (element.value !== "") {
+        element.style.border = "none";
+    }
 }
